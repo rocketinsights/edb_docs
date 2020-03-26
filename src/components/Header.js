055069819322bdadd2dfc72618a2268f023084df
Ed Lewis <ed.lewis@gmail.com>
2020-03-26 12:55:54 -0400

    remove search

--- src/components/Header.js
+++ src/components/Header.js
@@ -79,10 +79,6 @@ const Header = ({ location, isDarkThemeActive }) => (
 
       const twitter = require('./images/twitter.svg');
 
-      const discordBrandsBlock = require('./images/discord-brands-block.svg');
-
-      const twitterBrandsBlock = require('./images/twitter-brands-block.svg');
-
       const {
         site: {
           siteMetadata: { headerTitle, githubUrl, helpUrl, tweetText, logo, headerLinks },
