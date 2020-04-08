836e0e038627b0f7470a56ef30d3766d114a7407
Ed Lewis <ed.lewis@gmail.com>
2020-04-08 15:42:51 -0400

    add more product groups

--- content/frontlinks.js
+++ content/frontlinks.js
@@ -7,4 +7,44 @@ export default [
       { name: 'User Guide', link: '/epas/edb_plus_guide' },
     ],
   },
+  {
+    name: 'EDB Postgres Ark Platform',
+    links: [
+      { name: 'Installation Guide for Linux', link: '/epas/epas_inst_linux' },
+      { name: 'Installation Guide for Windows', link: '/epas/epas_inst_windows' },
+      { name: 'User Guide', link: '/epas/edb_plus_guide' },
+    ],
+  },
+  {
+    name: 'EDB Postgres Backup and Recovery Tool',
+    links: [
+      { name: 'Installation Guide for Linux', link: '/epas/epas_inst_linux' },
+      { name: 'Installation Guide for Windows', link: '/epas/epas_inst_windows' },
+      { name: 'User Guide', link: '/epas/edb_plus_guide' },
+    ],
+  },
+  {
+    name: 'EDB Postgres Cloud Database Service',
+    links: [
+      { name: 'Installation Guide for Linux', link: '/epas/epas_inst_linux' },
+      { name: 'Installation Guide for Windows', link: '/epas/epas_inst_windows' },
+      { name: 'User Guide', link: '/epas/edb_plus_guide' },
+    ],
+  },
+  {
+    name: 'EDB Postgres Enterprise Manager',
+    links: [
+      { name: 'Installation Guide for Linux', link: '/epas/epas_inst_linux' },
+      { name: 'Installation Guide for Windows', link: '/epas/epas_inst_windows' },
+      { name: 'User Guide', link: '/epas/edb_plus_guide' },
+    ],
+  },
+  {
+    name: 'EDB Postgres Failover Manager',
+    links: [
+      { name: 'Installation Guide for Linux', link: '/epas/epas_inst_linux' },
+      { name: 'Installation Guide for Windows', link: '/epas/epas_inst_windows' },
+      { name: 'User Guide', link: '/epas/edb_plus_guide' },
+    ],
+  },
 ];
