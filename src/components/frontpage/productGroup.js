4b9071adc42e16f24ffbe59aeb62a7a8c72359aa
Ed Lewis <ed.lewis@gmail.com>
2020-04-08 15:46:09 -0400

    start with product tiles

--- src/components/frontpage/productGroup.js
+++ src/components/frontpage/productGroup.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Link from '../link';
+import styled from '@emotion/styled';
+
+const ProductTile = styled('div')`
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.04);
+  /* width: calc((100% / 3) - 40px); */
+  width: 300px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  padding: 20px 15px 10px 15px;
+  border: 1px solid rgba(0, 0, 0, 0.07);
+  border-radius: 5px;
+  flex: 1;
+`;
+
+const ProductTitle = styled('div')`
+  font-weight: 700;
+  padding-bottom: 10px;
+`;
 
 const ProductGroup = ({ name, links }) => (
-  <>
-    {name}
+  <ProductTile>
+    <ProductTitle>{name}</ProductTitle>
     {links.map(link => {
       return (
         <div key={link.name}>
@@ -11,7 +29,7 @@ const ProductGroup = ({ name, links }) => (
         </div>
       );
     })}
-  </>
+  </ProductTile>
 );
 
 export default ProductGroup;
--- src/components/frontpage/productGroups.js
+++ src/components/frontpage/productGroups.js
@@ -1,13 +1,19 @@
 import React from 'react';
 // import Link from '../link';
 import ProductGroup from './productGroup';
+import styled from '@emotion/styled';
+
+const FlexGroups = styled('div')`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const ProductGroups = ({ linkGroups }) => (
-  <>
+  <FlexGroups>
     {linkGroups.map(linkGroup => {
       return <ProductGroup key={linkGroup.name} name={linkGroup.name} links={linkGroup.links} />;
     })}
-  </>
+  </FlexGroups>
 );
 
 export default ProductGroups;
