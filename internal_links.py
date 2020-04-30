521eba04e3f4905127be880482e9560bec6322a1
Ed Lewis <ed.lewis@gmail.com>
2020-04-30 13:03:10 -0400

    find all relative links

--- internal_links.py
+++ internal_links.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 import os
 import shutil
+import re
 
 links = {}
+
+for path in Path('content_build').rglob('*.mdx'):
+    root_path = str(path.parents[0]) + '/'
+    content_path = str(path.parents[2]) + '/'
+
+    f = path.open()
+    for line in f.readlines():
+      if "registered_link" in line:
+        id = "#" + line.split('"')[1]
+        links[id] = str(path.parent) + "/" + str(path.stem) + id
+print(links)
 
 for path in Path('content_build').rglob('*.mdx'):
     root_path = str(path.parents[0]) + '/'
@@ -11,10 +23,9 @@ for path in Path('content_build').rglob('*.mdx'):
 
     f = path.open()
     for line in f.readlines():
-      if "registered_link" in line:
-        id = line.split('"')[1]
-        links[id] = str(path.parent)
+      if "](#" in line:
+        tag = re.search('(?<=\()(.*?)(?=\))', line).group(0)
+        print(tag)
         print(line)
 
 
-print(links)
