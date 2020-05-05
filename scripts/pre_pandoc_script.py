c05791f662d6c5fb43ce33fb48000c9c64420f6e
Ed Lewis <ed.lewis@gmail.com>
2020-05-05 14:38:43 -0400

    more changes

--- scripts/pre_pandoc_script.py
+++ scripts/pre_pandoc_script.py
@@ -4,7 +4,6 @@ for line in fileinput.input(inplace=1):
   if line.startswith('.. _'):
     target = line.replace(".. _", "").replace(":\n", "")
     print('.. raw:: html\n\n<div id="' + target + '" class="registered_link"></div>\n')
-  elif "Y{0" not in line:
-    print(line.replace("\n", "").replace("™", "").replace("®", ""))
-  else:
-    print(line, end="")
+  elif not line.startswith(".. tabularcolumns"):
+    print(line.replace("™", "").replace("®", ""), end="")
+
