4af34bac5b07f20f2ffb20874c3c8e972a8cf078
Ed Lewis <ed.lewis@gmail.com>
2020-05-07 12:25:12 -0400

    remigrate

--- scripts/pre_pandoc_script.py
+++ scripts/pre_pandoc_script.py
@@ -2,14 +2,15 @@ import fileinput
 import re
 
 def clean_up_ref(ref):
-  new_ref = ref.replace("(", "").replace(")", "").replace("_>", ">")
+  new_ref = ref.replace("(", "").replace(")", "").replace("_>", ">").replace("/","_")
   return new_ref
 
 def extract_target(line):
-  target = line.replace(".. _", "").replace(":\n", "").replace("(", "").replace(")", "")
+  target = line.replace(".. _", "").replace(":\n", "").replace("(", "").replace(")", "").replace("/","_")
   if target[-1] == "_":
     target = target[0:-1]
   return target
+
 
 for line in fileinput.input(inplace=1):
   new_line = line.replace("™", "").replace("®", "")
@@ -17,7 +18,8 @@ for line in fileinput.input(inplace=1):
     target = extract_target(line)
     print('.. raw:: html\n\n<div id="' + target + '" class="registered_link"></div>\n')
   elif not line.startswith(".. tabularcolumns"):
-    ref_links = re.findall('\:ref\:\`[.0-9a-zA-Z\s\-]* \<(.*?\>\`)', line)
+    ref_links = re.findall('\:ref\:\`[.0-9a-zA-Z\s\-]*\<(.*?\>\`)', line)
+
     for ref in ref_links:
       new_line = new_line.replace(ref, "#" + clean_up_ref(ref) + "_")
     new_line = new_line.replace(":ref:", "")
