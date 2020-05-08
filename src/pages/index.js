670c712f7431eeada6d511d1bb03e9559bbb65ba
John <jtrocchi1@gmail.com>
2020-05-08 12:34:54 -0400

    add layout for posts as well as index

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,26 +40,26 @@ const navStyles = {
 export default () => {
   return (
     <Layout>
-        <Row>
-          <PaddedCol style={{height: "100vh" }} md={2} className="border-right">
-            <EdbLogo />
-            <IndexLinks indexLinkList={indexLinkList} />
-          </PaddedCol>
-          <Col className="m-0 p-0">
-            <Navbar className="border-bottom fluid" style={navStyles}>
-              <SearchBar />
-            </Navbar>
-            <Container>
-              <h1>Welcome To EDB Docs</h1>
-              <ProductGroups linkGroups={linkGroups} />
-              <HeadlineWithStrap
-                headline=" Learn EDB Postgres"
-                strap="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, asperiores!"
-              />
-              <ArticleStubs articles={articles} />
-            </Container>
-          </Col>
-        </Row>
+      <Row>
+        <PaddedCol style={{height: "100vh" }} md={2} className="border-right">
+          <EdbLogo />
+          <IndexLinks indexLinkList={indexLinkList} />
+        </PaddedCol>
+        <Col className="m-0 p-0">
+          <Navbar className="border-bottom fluid" style={navStyles}>
+            <SearchBar />
+          </Navbar>
+          <Container>
+            <h1>Welcome To EDB Docs</h1>
+            <ProductGroups linkGroups={linkGroups} />
+            <HeadlineWithStrap
+              headline=" Learn EDB Postgres"
+              strap="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, asperiores!"
+            />
+            <ArticleStubs articles={articles} />
+          </Container>
+        </Col>
+      </Row>
     </Layout>
   );
 };
