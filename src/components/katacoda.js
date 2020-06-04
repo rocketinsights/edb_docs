c79ad7eec83b9381875e07095b8cdffd52eeb7a1
Josh Heyer <63653723+josh-heyer@users.noreply.github.com>
2020-06-04 14:13:48 -0600

    Add Katacoda embed component
    For advocacy docs. Usage in .mdx:
    
    import Katacoda from '../../../../src/components/katacoda';
    
    ... some markdown ...
    
    <Katacoda account="shog9" scenario="java-jdbc" />

---
+++ src/components/katacoda.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Helmet } from "react-helmet"
+
+const Katacoda = ({account, scenario, highlightColor="e94621", height='600px'}) => (
+    <>
+        <Helmet>
+            <script src="https://katacoda.com/embed.js" />
+        </Helmet>
+        <div id={`katacoda-scenario-${account}-${scenario}`}
+            data-katacoda-id={`${account}/${scenario}`}
+            data-katacoda-color={highlightColor}
+            style={{height: height, paddingTop: '20px'}}></div>
+    </>
+);
+  
+export default Katacoda;
