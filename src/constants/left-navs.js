9fb50f937896a0ddcfc0decd4d9cb780a8c49d61
Robert Stringer <robert.stringer@enterprisedb.com>
2020-06-09 11:48:17 +0100

    Updates to left-nav.js; tweaks to navTitles to be more consistent w/ EPASv12

--- src/constants/left-navs.js
+++ src/constants/left-navs.js
@@ -44,5 +44,52 @@ export const leftNavs = {
         path: 'language_pack',
       },
     ],
+    '11' : [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: '01_epas_inst_linux',
+      },
+      {
+        path: '02_epas_inst_windows',
+      },
+      {
+        path: '03_epas_release_notes',
+      },
+      {
+        path: '04_epas_upgrade_guide',
+      },
+      {
+        path: null,
+        title: 'For Oracle Developers',
+      },
+      {
+        path: '05_epas_compat_ora_dev_guide',
+      },
+      {
+        path: '06_epas_compat_bip_guide',
+      },
+      {
+        path: '07_epas_compat_ora_dev_guide',
+      },
+      {
+        path: '08_epas_compat_tools_guide',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: '11_epas_guide',
+      },
+      {
+        path: '09_ecpgplus_guide',
+      },
+      {
+        path: '10_language_pack',
+      },
+    ],
   },
 };
