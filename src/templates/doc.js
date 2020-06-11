e135fe22f18571a486682a65bb243e4ce99bcc5b
Robert Stringer <robert.stringer@enterprisedb.com>
2020-06-11 19:45:31 +0100

    making it so that the navTitle is used in the excerpt link

--- src/templates/doc.js
+++ src/templates/doc.js
@@ -105,19 +105,19 @@ const Section = ({ section }) => (
     <div className="card rounded shadow-sm p-2">
       <div className="card-body">
         <h3 className="card-title balance-text">{section.title}</h3>
-        {/* {section.guides.map(guide => (
+        {section.guides.map(guide => (
           <p className="card-text" key={`${guide.frontmatter.title}`}>
             <Link
               to={guide.fields.path}
               className="btn btn-link btn-block text-left p-0"
             >
-              {guide.frontmatter.title}
+              {guide.frontmatter.navTitle}
             </Link>
             <span className="small text-muted">
               {guide.frontmatter.description || guide.excerpt}
             </span>
           </p>
-        ))} */}
+        ))}
       </div>
     </div>
   </div>
