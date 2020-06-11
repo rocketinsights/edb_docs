5e40646f7d44dd3353c8f92372f91d7c24934c74
Evan Barger <epbarger@gmail.com>
2020-06-11 11:04:34 -0400

    Update icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -242,7 +242,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /icons/,
+          include: /static/,
         },
       },
     },
