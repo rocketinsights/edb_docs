9f9a3b461c97f67532e6c3eab3ffce08dc17f7b2
Ed Lewis <ed.lewis@gmail.com>
2020-06-26 12:28:56 -0400

    update files

--- scripts/mdx2pdf.sh
+++ scripts/mdx2pdf.sh
@@ -4,7 +4,7 @@ pandoc "$1" \
     --toc \
     -f gfm \
     --include-in-header scripts/inline_code.tex \
-    --highlight-style espresso \
+    --highlight-style scripts/test.theme \
     -V linkcolor:blue \
     -V geometry:a4paper \
     -V geometry:margin=2cm \
@@ -12,3 +12,5 @@ pandoc "$1" \
     -V monofont="Monaco" \
     --pdf-engine=xelatex \
     -o "$2" 
+
+# scripts/mdx2pdf.sh source.mdx output.pdf
