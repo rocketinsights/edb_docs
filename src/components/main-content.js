6587e03c6340037326496f6febe17fe60ae866e0
Evan Barger <epbarger@gmail.com>
2020-06-30 15:59:44 -0400

    Add styled search bar to advanced search page

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import SearchNavigation from './search-navigation';
 
-const MainContent = ({ children, search = true }) => {
+const MainContent = ({ children }) => {
   return (
     <div className="flex-grow-1 border-right min-w-50">
-      { search && <SearchNavigation /> }
-      <main role="main" className={`mt-0 ${search ? 'p-5' : 'pt-4 pl-5 pr-5 pb-5'}`}>
+      <SearchNavigation />
+      <main role="main" className="mt-0 p-5">
         {children}
       </main>
     </div>
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Navbar, Button } from 'react-bootstrap';
 import { indexLinkList } from '../constants/index-link-list';
 import Icon, { iconNames } from '../components/icon/';
 import algoliasearch from 'algoliasearch/lite';
@@ -11,6 +11,8 @@ import {
   Pagination,
   RefinementList,
   ClearRefinements,
+  connectSearchBox,
+  Configure,
 } from 'react-instantsearch-dom';
 import {
   Footer,
@@ -20,6 +22,13 @@ import {
   SideNavigation,
   TopBar,
 } from '../components';
+import {
+  AdvancedSearchTabLink,
+  SearchTab,
+  SlashIndicator,
+  ClearButton,
+  SearchPane,
+} from '../components/search/formComps';
 import { PageHit } from '../components/search/hitComps'
 
 import { graphql } from 'gatsby';
@@ -48,9 +57,56 @@ export const query = graphql`
   }
 `;
 
+const AdvancedSearchFiltering = () => (
+  <>
+    <div>Content Area</div>
+    <div>Product</div>
+    <RefinementList attribute='product' />
+    <div>Version</div>
+    <RefinementList attribute='version' />
+    <ClearRefinements />
+  </>
+);
+
+const AdvancedSearchResults = () => (
+  <>
+    <Index key={learnIndex.index} indexName={learnIndex.index} >
+      <Hits hitComponent={PageHit} />
+    </Index>
+    <Index key={docsIndex.index} indexName={docsIndex.index} >
+      <Hits hitComponent={PageHit} />
+    </Index>
+    <hr/>
+    <Pagination/>
+  </>
+);
+
+const AdvancedSearchInput = ({currentRefinement, refine, query}) => {
+  const queryLength = (query || '').length;
+
+  return (
+    <form noValidate action="" autoComplete="off" role="search" className={`w-100 search-form d-flex align-items-center`}>
+      <Icon iconName={iconNames.SEARCH} className="fill-black ml-3 opacity-5 flex-shrink-0" width="22" height="22" />
+      <input
+        id='search-input'
+        className="form-control form-control-lg border-0 pl-3"
+        type="text"
+        aria-label="search"
+        placeholder="Search"
+        value={currentRefinement}
+        onChange={e => refine(e.currentTarget.value)}
+      />
+      <ClearButton onClick={() => { refine('') }} className={`${queryLength === 0 && 'd-none'}`} />
+    </form>
+  );
+};
+const AdvancedSearchBox = connectSearchBox(AdvancedSearchInput);
+
 export default data => {
   const advocacyLinks =
     data.data.file.childAdvocacyDocsJson.advocacyLinks || [];
+  const [query, setQuery] = useState(``);
+
   return (
     <Layout background='white'>
       <TopBar />
@@ -58,28 +114,30 @@ export default data => {
         <InstantSearch
           searchClient={searchClient}
           indexName={docsIndex.index}
+          onSearchStateChange={({ query }) => setQuery(query)}
         >
-          <SideNavigation background='white' footer={false}>
-            <div>Content Area</div>
-            <div>Product</div>
-            <RefinementList attribute='product' />
-            <div>Version</div>
-            <RefinementList attribute='version' />
-            <ClearRefinements />
-          </SideNavigation>
-          <MainContent search={false}>
-            <SearchBox />
-            <Index key={learnIndex.index} indexName={learnIndex.index} >
-              <Hits hitComponent={PageHit} />
-            </Index>
-            <hr/>
-            <Index key={docsIndex.index} indexName={docsIndex.index} >
-              <Hits hitComponent={PageHit} />
-            </Index>
+          <Configure hitsPerPage={30} />
 
-            <Pagination/>
-            <Footer />
-          </MainContent>
+          <SideNavigation background='white' footer={false}>
+            <AdvancedSearchFiltering />
+          </SideNavigation>
+
+          <div className="flex-grow-1 border-right min-w-50">
+            <Navbar variant="light" className="flex-md-nowrap p-3">
+              <AdvancedSearchBox query={query} />
+              <Button variant="link" className="text-nowrap mr-2 ml-3">
+                Support
+              </Button>
+              <Button variant="link" className="text-nowrap mr-2">
+                Sign In
+              </Button>
+            </Navbar>
+
+            <main role="main" className="mt-0 p-3">
+              <AdvancedSearchResults />
+              <Footer />
+            </main>
+          </div>
         </InstantSearch>
       </Container>
     </Layout>
