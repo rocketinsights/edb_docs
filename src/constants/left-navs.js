3076f7ca5b6a59712cd2448551eaa5f61bbd8903
Robert Stringer <robert.stringer@enterprisedb.com>
2020-09-11 20:31:23 +0100

    v11 tidy up

--- src/constants/left-navs.js
+++ src/constants/left-navs.js
@@ -54,6 +54,54 @@ export const leftNavs = {
         path: 'epas_guide',
       },
     ],
+    '11': [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: 'epas_inst_linux',
+      },
+      {
+        path: 'epas_inst_windows',
+      },
+      {
+        path: 'epas_upgrade_guide',
+      },
+      {
+        path: null,
+        title: 'For Oracle Developers',
+      },
+      {
+        path: 'epas_compat_ora_dev_guide',
+      },
+      {
+        path: 'epas_compat_reference',
+      },
+      {
+        path: 'epas_compat_bip_guide',
+      },
+      {
+        path: 'epas_compat_tools_guide',
+      },
+      {
+        path: null,
+        title: 'Tools',
+      },
+      {
+        path: 'ecpgplus_guide',
+      },
+      {
+        path: 'language_pack',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: 'epas_guide',
+      },
+    ],
   },
 
   ark: {
