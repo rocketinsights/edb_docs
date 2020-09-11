2d0a88afb2d16dc2b26837e89960d439f3cedf47
Evan Barger <epbarger@gmail.com>
2020-09-11 09:39:37 -0400

    Change PdfDownload so that it works within docs too

--- src/components/pdf-download.js
+++ src/components/pdf-download.js
@@ -13,14 +13,15 @@ const PdfDownload = ({ path }) => {
     }
   `);
 
+  const productPath = path.split('/').slice(0,3).join('/');
   const file = data.allFile.nodes.find(
-    pdf => `/${pdf.relativeDirectory}` === path,
+    pdf => `/${pdf.relativeDirectory}` === productPath,
   );
 
   if (file) {
     return (
       <div>
-        <span class="mt-3 mb-2 font-weight-bold text-muted text-uppercase small d-block">
+        <span className="mt-3 mb-2 font-weight-bold text-muted text-uppercase small d-block">
           Other Versions
         </span>
         <a href={file.publicURL} className="download-pdf pl-0">
