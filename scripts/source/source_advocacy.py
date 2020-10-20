287a07d3d1d3df0beab94de3c8f0312b4fadc747
Evan Barger <epbarger@gmail.com>
2020-10-20 11:43:20 -0400

    Refactor to only be docs and conversion scripts

--- scripts/source/source_advocacy.py
+++
@@ -1,7 +0,0 @@
-import os
-import shutil
-
-print('sourcing advocacy_docs')
-if os.path.exists('advocacy_docs/advocacy_docs'):
-    shutil.rmtree('advocacy_docs')
-os.system('git clone https://github.com/rocketinsights/edb_docs_advocacy.git advocacy_docs')
--- src/components/dev-only.js
+++
@@ -1,8 +0,0 @@
-const DevOnly = ({ children }) => {
-  if (process.env.NODE_ENV === 'development') {
-    return children;
-  }
-  return null;
-};
-
-export default DevOnly;
