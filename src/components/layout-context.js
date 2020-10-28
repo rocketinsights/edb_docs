0ad00aa390392f98985dd4220b4666631680fbca
Evan Barger <epbarger@gmail.com>
2020-10-28 11:02:05 -0400

    The Big Swap! Merge complete rework in. (#183)
    * Refactor to only be docs and conversion scripts
    
    * Remove playground

--- src/components/layout-context.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default React.createContext({
-  dark: false,
-  toggleDark: () => {},
-})
--- src/components/logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LogoSvg from '../../static/edb-icons/edb-docs-logo-disc-dark.svg';
-
-function Logo({ className, width, height }) {
-  return <LogoSvg className={className} height={height} width={width} />;
-}
-
-export default Logo;
